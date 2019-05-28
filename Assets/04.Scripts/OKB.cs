a9832194233a0ed1418eede4364f4e5a4aea1c2f
Wonchaeyeon <s2018w34@e-mirim.hs.kr>
2019-05-28 22:50:36 +0900

    게임 방법, 브금, 배경 추가했음

---
+++ Assets/04.Scripts/OKB.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OKB : MonoBehaviour
+{
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("Title"); //너가 넘기고 싶은 씬
+    }
+}
